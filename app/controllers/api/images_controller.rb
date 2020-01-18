7917b7b3eddfc183fc8fa186c2b985667f9bdb93
dianakakoma <dikakoma@gmail.com>
2020-01-17 23:29:04 -0600

    installed the Cloundinay API

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -3,18 +3,34 @@ class Api::ImagesController < ApplicationController
     @image = current_user.property.id.image
   end
 
-  def create
-    @image = Image.new(
-      property_id: params[:property_id],
-      url: params[:url],
-      user_id: params[:user_id],
-    )
-    @image.save
+  #def create
+  #@image = Image.new(
+  #property_id: params[:property_id],
+  #url: params[:url],
+  #user_id: params[:user_id],
+  #)
+  #@image.save
 
-    if @image.save
-      render "show.json.jb"
+  #if @image.save
+  #render "show.json.jb"
+  #else
+  #render json: { errors: @image.errors.full_messages }, status: 422
+
+  #end
+  #end
+
+  def create
+    response = Cloudinary::Uploader.upload(params[:image])
+    cloudinary_url = response["secure_url"]
+    @post = Post.new(
+      title: params[:title],
+      body: params[:body],
+      image: cloudinary_url,
+    )
+    if @post.save
+      render "show.json.jbuilder"
     else
-      render json: { errors: @image.errors.full_messages }, status: 422
+      render json: { errors: @post.errors.full_messages }, status: 422
     end
   end
 end
