eaf099f412eca63f6b7439846e08a0a742aca263
dianakakoma <dikakoma@gmail.com>
2020-01-22 19:39:05 -0600

    optics only

--- app/controllers/api/amenity_users_controller.rb
+++ app/controllers/api/amenity_users_controller.rb
@@ -13,6 +13,14 @@ class Api::AmenityUsersController < ApplicationController
     render json: { message: "Success!" }
   end
 
+  def create_all
+    current_user.amenity_users.destroy_all
+    params[:amenity_ids].each do |amenity_id|
+      AmenityUser.create(amenity_id: amenity_id, user_id: current_user.id)
+    end
+    render json: { message: "success" }
+  end
+
   def destroy
     @amenity_user = AmenityUser.find_by(id: params[:id])
     @amenity_user.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 
     get "/amenity_users" => "amenity_users#index"
     post "/amenity_users" => "amenity_users#create"
+    post "/amenity_users/all" => "amenity_users#create_all"
     delete "/amenity_users/:id" => "amenity_users#destroy"
 
     get "/images" => "images#index"
